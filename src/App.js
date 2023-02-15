6d87c1c2d2874e1f5b0b0167e7f05e44657e98b4
Tushar Deshmukh <50114103+DeshmukhTushar222@users.noreply.github.com>
2023-02-15 11:37:36 +0530

    Redux form basic example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import {
+  increaseCounter,
+  decreaseCounter,
+} from "./redux/Counter/counter.actions"
+import { reduxForm } from "redux-form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const {
+      fields: { firstName, lastName },
+      increaseCounter, decreaseCounter,
+      count
+    } = this.props;
+    return (
+      <div>
+        <div>
+          <label>First Name</label>
+          <input type="text" {...firstName} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type="text" {...lastName} />
+        </div>
+        <div>Count: {count}</div>
+        <button onClick={() => {
+          console.log('call came to me ~')
+          increaseCounter()
+          console.log('Fuckkkk')
+        }}>
+          Increase Count Form
+        </button>
+        <button onClick={() => decreaseCounter()}>
+          Decrease Count Form
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  };
+};
+
+const appForm = reduxForm(
+  {
+    form: "App Form",
+    fields: ["firstName", "lastName"],
+  },
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default appForm
---
+++ src/mainui.js
@@ -0,0 +1,14 @@
+import React from "react"
+import App1 from "./App1"
+import App from "./App"
+
+function App0(props) {
+  return (
+    <div className="App">
+      <App/>
+      <App1/>
+    </div>
+  )
+}
+
+export default App0
